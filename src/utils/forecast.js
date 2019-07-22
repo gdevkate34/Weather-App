b4815346c75420ba51ab8799014ab2b94dd6223b
gdevkate34 <gdevkate34@gmail.com>
2019-07-23 02:07:18 +0530

    changed forecst data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (long,lat,callback)=>{
         }else if(body.length===0){
             callback('Unable to find location!')
         }else{
-            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out.It noted Low temperature '+body.currently.temperatureLow +' degrees and high tempaerature, '+body.currently.temperatureHigh+' There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
